9c60662cce2b7e4363690567d84fae94a7a1d312
Nicolas Dubois <DuboisNicolasToulouse@users.noreply.github.com>
2024-11-29 08:58:47 +0100

    Simple main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,34 @@
-mod metadata;
+use clap::Parser;
 use metadata::Metadata;
+use std::io::Error;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
+mod metadata;
+
+#[derive(Parser)]
+struct Cli {
+    /// image to load
+    image: std::path::PathBuf,
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let args = Cli::parse();
+
+    let result = Metadata::new(&args.image);
+
+    if result.is_err() {
+        return Err(Error::other(format!(
+            "Cannot parse file '{}'",
+            args.image.display()
+        )));
+    }
+
+    let metadata = result.unwrap();
+    println!("File: {}", metadata.path().display());
+    println!("Dimensions: {}, {}", metadata.width(), metadata.height());
+    println!(
+        "Date: {}",
+        metadata.exif_date().unwrap_or("No date!".to_string())
+    );
+
     Ok(())
 }
