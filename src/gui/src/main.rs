d70db3819fec876eb8f2737ede7fa900e4e6b6ba
Nicolas Dubois <DuboisNicolasToulouse@users.noreply.github.com>
2024-12-21 15:31:17 +0100

    Add application icon

--- src/gui/src/main.rs
+++ src/gui/src/main.rs
@@ -1,6 +1,27 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
 use eframe::egui;
+use image::ImageReader;
+use std::{io::Cursor, sync::Arc};
 
 const PROJECT_NAME: &str = "PhotosNorm";
+
+fn load_icon() -> Option<Arc<egui::IconData>> {
+    let image_reader = ImageReader::new(Cursor::new(include_bytes!("../assets/icon.png")))
+        .with_guessed_format()
+        .ok()?;
+    match image_reader.decode() {
+        Ok(image) => Some(Arc::new(egui::IconData {
+            rgba: image.to_rgba8().to_vec(),
+            width: image.width(),
+            height: image.height(),
+        })),
+        Err(e) => {
+            println!("Failed to load icon: {}", e);
+            None
+        }
+    }
+}
 
 struct WelcomePannel {}
 
@@ -13,7 +34,14 @@ impl eframe::App for WelcomePannel {
 }
 
 fn main() -> eframe::Result {
-    let native_options = eframe::NativeOptions::default();
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder {
+            icon: load_icon(),
+            ..Default::default()
+        },
+        ..Default::default()
+    };
+
     eframe::run_native(
         PROJECT_NAME,
         native_options,
