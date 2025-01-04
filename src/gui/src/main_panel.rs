03ca26377b04952c741c91661fd1d64b2c109e08
Nicolas Dubois <DuboisNicolasToulouse@users.noreply.github.com>
2025-01-04 17:40:07 +0100

    main panel: display metadata

--- src/gui/src/main_panel.rs
+++ src/gui/src/main_panel.rs
@@ -1,11 +1,12 @@
 use crate::{assets, taffy_tools};
 use core::assets as core_assets;
+use core::metadata::Metadata;
 use eframe::egui;
 use egui_taffy::{
     self,
     taffy::{
         self,
-        prelude::{FromPercent, TaffyMinContent},
+        prelude::{FromLength, FromPercent},
     },
     TuiBuilderLogic,
 };
@@ -13,11 +14,14 @@ use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
 
 struct ImageData<'a> {
-    #[allow(dead_code)] // May be used later
+    #[allow(dead_code)] // TODO: May be used later
     path: &'a PathBuf,
-    uri: Result<String, std::io::Error>,
     filename: String,
     error: Option<String>,
+    uri: Option<String>,
+    #[allow(dead_code)] // TODO: May be used later
+    metadata: Option<Metadata>,
+    camera: String,
     description: String,
     date: String,
 }
@@ -51,16 +55,13 @@ impl State {
     }
 }
 
-trait TuiTool {
+trait UiTool {
     fn wrapped_label(&mut self, text: impl Into<egui::WidgetText>);
 }
 
-impl TuiTool for egui_taffy::Tui {
+impl UiTool for egui::Ui {
     fn wrapped_label(&mut self, text: impl Into<egui::WidgetText>) {
-        self.ui(|ui| {
-            ui.style_mut().wrap_mode = Some(egui::TextWrapMode::Wrap);
-            ui.label(text);
-        });
+        self.add(egui::Label::new(text).wrap_mode(egui::TextWrapMode::Wrap));
     }
 }
 
@@ -70,6 +71,12 @@ impl MainPanel<'_> {
         let images = image_paths
             .iter()
             .map(|path| {
+                let filename = path
+                    .file_name()
+                    .unwrap_or(std::ffi::OsStr::new("Unexpected invalid file name"))
+                    .to_string_lossy()
+                    .to_string();
+
                 let uri = match std::fs::canonicalize(path) {
                     Ok(path) => match path.to_str() {
                         Some(path_str) => {
@@ -82,24 +89,40 @@ impl MainPanel<'_> {
                     Err(e) => Err(e),
                 };
 
-                let error = match uri {
+                let metadata = match uri {
+                    Ok(_) => Metadata::new(path),
+                    Err(ref e) => Err(std::io::Error::other(e.to_string())),
+                };
+
+                let camera = match metadata {
+                    Ok(ref m) => m.camera_info().to_string(),
+                    Err(_) => String::new(),
+                };
+
+                let description = match metadata {
+                    Ok(ref m) => m.description().unwrap_or(String::new()),
+                    Err(_) => String::new(),
+                };
+
+                let date = match metadata {
+                    Ok(ref m) => m.exif_date().unwrap_or(String::new()),
+                    Err(_) => String::new(),
+                };
+
+                let error = match metadata {
                     Ok(_) => None,
                     Err(ref e) => Some(e.to_string()),
                 };
 
-                let filename = path
-                    .file_name()
-                    .unwrap_or(std::ffi::OsStr::new("Unexpected invalid file name"))
-                    .to_string_lossy()
-                    .to_string();
-
                 ImageData {
                     path,
-                    uri,
                     filename,
                     error,
-                    description: String::new(),
-                    date: String::new(),
+                    uri: uri.ok(),
+                    camera,
+                    metadata: metadata.ok(),
+                    description,
+                    date,
                 }
             })
             .collect::<Vec<_>>();
@@ -224,6 +247,8 @@ impl eframe::App for MainPanel<'_> {
                         ..Default::default()
                     })
                     .show(|tui| {
+                        let infos_width = tui.egui_ui().available_width() - state.images_size_vec.x - 20.0;
+
                         for image in &mut self.images {
                             tui.style(taffy::Style {
                                 flex_direction: taffy::FlexDirection::Row,
@@ -243,7 +268,7 @@ impl eframe::App for MainPanel<'_> {
                                 })
                                 .ui(|ui| {
                                     match &image.uri {
-                                        Ok(uri) => {
+                                        Some(uri) => {
                                             //TODO: Image are not loaded in background.
                                             let image_widget = egui::Image::from_uri(uri);
                                             let result =
@@ -266,7 +291,7 @@ impl eframe::App for MainPanel<'_> {
                                                 }
                                             };
                                         }
-                                        Err(_) => {
+                                        None => {
                                             ui.add_sized(
                                                 state.images_size_vec,
                                                 egui::widgets::Image::new(assets::ERROR_IMG),
@@ -274,46 +299,55 @@ impl eframe::App for MainPanel<'_> {
                                         }
                                     };
                                 });
+
                                 tui.style(taffy::Style {
                                     size: taffy::Size {
-                                        width: taffy::Dimension::from_percent(1.0),
+                                        width: taffy::Dimension::from_length(infos_width),
                                         height: taffy::Dimension::from_percent(0.0),
                                     },
-                                    display: taffy::Display::Grid,
-                                    grid_template_columns: vec![
-                                        taffy::TrackSizingFunction::MIN_CONTENT,
-                                        taffy::prelude::fr(1.0),
-                                    ],
-                                    gap: taffy_tools::size_lp(4.0, 10.0),
-
+                                    display: taffy::Display::Block,
                                     ..Default::default()
                                 })
                                 .add(|tui| {
-                                    tui.label("File:");
-                                    tui.wrapped_label(&image.filename);
+                                    tui.ui(|ui| {
+                                        egui::Grid::new(&image.filename)
+                                            .num_columns(2)
+                                            .spacing(egui::Vec2::new(4.0, 10.0))
+                                            .show(ui, |ui| {
+                                                ui.label("File:");
+                                                ui.label(&image.filename);
+                                                ui.end_row();
 
-                                    match image.error {
-                                        Some(ref e) => {
-                                            tui.label("Error:");
-                                            tui.wrapped_label(e);
-                                        }
-                                        None => {
-                                            tui.label("Camera:");
-                                            tui.wrapped_label("TODO CAMERA INFO");
+                                                match image.error {
+                                                    Some(ref e) => {
+                                                        ui.label("Error:");
+                                                        ui.wrapped_label(e);
+                                                        ui.end_row();
+                                                    }
+                                                    None => {
+                                                        ui.label("Camera:");
+                                                        ui.wrapped_label(&image.camera);
+                                                        ui.end_row();
 
-                                            tui.label("Description: ");
-                                            tui.ui_add(
-                                                egui::TextEdit::singleline(&mut image.description)
-                                                    .desired_width(f32::INFINITY),
-                                            );
+                                                        ui.label("Description: ");
+                                                        ui.add(
+                                                            egui::TextEdit::singleline(
+                                                                &mut image.description,
+                                                            )
+                                                            .desired_width(f32::INFINITY),
+                                                        );
+                                                        ui.end_row();
 
-                                            tui.label("Date: ");
-                                            tui.ui_add(
-                                                egui::TextEdit::singleline(&mut image.date)
-                                                    .desired_width(f32::INFINITY),
-                                            );
-                                        }
-                                    }
+                                                        ui.label("Date: ");
+                                                        ui.add(
+                                                            egui::TextEdit::singleline(&mut image.date)
+                                                                .desired_width(f32::INFINITY),
+                                                        );
+                                                        ui.end_row();
+                                                    }
+                                                }
+                                            });
+                                    });
                                 });
                             });
                             tui.separator();
